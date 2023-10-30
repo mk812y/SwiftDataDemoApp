b0906d762099abc6b5fa322cf9dc4f4ee4f24ccf
mk812y <mk812y@gmail.com>
2023-10-30 21:00:26 +0100

    add disabled and tips

---
+++ SwiftDataDemoApp/AddStudentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddStudentView.swift
+//  SwiftDataDemoApp
+//
+//  Created by Михаил Куприянов on 30.10.23..
+//
+
+import SwiftUI
+
+struct AddStudentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddStudentView()
+}
---
+++ SwiftDataDemoApp/StudentListView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentListView.swift
+//  SwiftDataDemoApp
+//
+//  Created by Михаил Куприянов on 30.10.23..
+//
+
+import SwiftUI
+
+struct StudentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentListView()
+}
